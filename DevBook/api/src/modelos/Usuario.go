15222fbcd98f54c10c4a87e5572d6f270a75117e
Clevenielson <clevenielson@gmail.com>
2021-05-02 16:41:18 -0300

    add novas implementações Devbook

--- DevBook/api/src/modelos/Usuario.go
+++ DevBook/api/src/modelos/Usuario.go
@@ -1,6 +1,11 @@
 package modelos
 
-import "time"
+import (
+  //"fmt"
+  "errors"
+  "time"
+  "strings"
+)
 
 // Usuario representa um usuario utilizando a rede social
 type Usuario struct {
@@ -10,4 +15,40 @@ type Usuario struct {
   Email    string          `json:"email,omitempty"`
   Senha    string          `json:"senha,omitempty"`
   CriadoEm time.Time       `json:"CriadoEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar o usuario recebido
+func (usuario *Usuario) Preparar(etapa string) error {
+  if erro := usuario.validar(etapa); erro != nil {
+    return erro
+  }
+
+  usuario.formatar()
+  return nil
+}
+
+func (usuario *Usuario) validar(etapa string) error {
+  if usuario.Nome == "" {
+    return errors.New("O nome é obrigatório e não pode estar em branco")
+  }
+
+  if usuario.Email == "" {
+    return errors.New("O email é obrigatório e não pode estar em branco")
+  }
+
+  if usuario.Nick == "" {
+    return errors.New("O nick é obrigatório e não pode estar em branco")
+  }
+
+  if etapa == "cadastro" && usuario.Senha == ""   {
+    return errors.New("A senha é obrigatória e não pode estar em branco")
+  }
+  
+  return nil
+}
+
+func (usuario *Usuario) formatar() {
+  usuario.Nome = strings.TrimSpace(usuario.Nome)
+  usuario.Nick = strings.TrimSpace(usuario.Nick)
+  usuario.Email = strings.TrimSpace(usuario.Email)
 }
